7bab3f3fa8658445163a03ecf8a1d2650bf8fad9
juhlianna <jupanini31@gmail.com>
2022-05-06 14:17:46 -0300

    Initial commit

---
+++ src/main/java/com/digitalinnovtionone/springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovtionone.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
